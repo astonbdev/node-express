53ea9045e13585ad60670293270d875378aba8f5
Brian Aston <astonb.dev@gmail.com>
2022-02-16 11:38:35 -0800

    Finished primary mean routing. Need to add error handling

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const app = express();
 
 // useful error class to throw
 const { NotFoundError } = require("./expressError");
+const {findMean, findMode, findMedian} = require("./stats.js");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function(req, res){
+  //console.log("req.query", req.query);
+  //console.log("req.params.nums", req.params.nums);
+  let passedNums = req.query.nums;
+  passedNums = passedNums.split(",");
+  passedNums = passedNums.map(num => Number(num));
+  //console.log(passedNums);
+  const mean = findMean(passedNums);
 
+  return res.json({
+    operation: "mean",
+    value: mean
+  });
+
+});
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
 
